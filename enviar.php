3f3d944866308706f6b235f23a43707aca8a6a32
naualt08 <80216447+naualt08@users.noreply.github.com>
2021-03-07 17:48:55 -0300

    Add files via upload

--- enviar.php
+++ enviar.php
@@ -20,6 +20,6 @@ $carta .= "rta 3: $respuesta3 \n";
 $carta .= "rta 4: $respuesta4 \n";
 // Enviando Mensaje
 mail($destinatario, $asunto, $carta);
-header('Location:mensaje-de-envio.html');
+header('Location:https://naualt08.github.io/msj/')
 
 ?>
